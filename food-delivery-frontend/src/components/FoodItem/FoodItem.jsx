2cdc81760b9b707368837621d73460f538b7f06a
ngynbngoc <95579254+ngynbngoc@users.noreply.github.com>
2024-09-07 09:22:15 +0700

    change StoreContext

--- food-delivery-frontend/src/components/FoodItem/FoodItem.jsx
+++ food-delivery-frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,40 +1,52 @@
-import React, {useState} from 'react'
-import "./FoodItem.css"
-import {assets} from '../../assets/assets'
+import React, { useContext, useState } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
+const FoodItem = ({ id, name, price, desc, image }) => {
+    // const [itemCount, setItemCount] = useState(0);
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
-const FoodItem = ({id, name, price, desc, image}) => {
+    return (
+        <div className="food-item-container  w-100 shadow mb-4 bg-body rounded-3">
+            <div className="food-item-img-container ">
+                <img
+                    src={image}
+                    className="food-item-image w-100 rounded-top-2 "
+                />
+                {!cartItems[id] ? (
+                    <img
+                        className="add-button"
+                        onClick={() => addToCart(id)}
+                        src={assets.add_icon_white}
+                    />
+                ) : (
+                    <div className="food-item-counter">
+                        <img
+                            onClick={() => removeFromCart(id)}
+                            src={assets.remove_icon_red}
+                        />
 
-    const [itemCount , setItemCount] = useState(0)
+                        <p>{cartItems[id]}</p>
 
-  return (
-    
-    <div className='food-item-container  w-100 shadow mb-4 bg-body rounded-3'>
-      <div className='food-item-img-container '>
-        <img src={image} className='food-item-image w-100 rounded-top-2 '/>
-        {!itemCount
-                ?<img className='add-button' onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_white} />
-                :<div className='food-item-counter'>
-                    <img onClick={() => setItemCount(prev => prev-1)} src={assets.remove_icon_red} />
-                    
-                    <h7>{itemCount}</h7>
+                        <img
+                            onClick={() => addToCart(id)}
+                            src={assets.add_icon_green}
+                        />
+                    </div>
+                )}
+            </div>
 
-                    <img onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_green} />
+            <div className="food-item-info p-3 ">
+                <div className=" d-flex justify-content-between align-items-center">
+                    <p className="fs-6 fw-bold mt-3">{name}</p>
+                    <img src={assets.rating_starts} />
                 </div>
-        }
-      </div>
-
-      <div className='food-item-info p-3 '>
-        <div className=' d-flex justify-content-between align-items-center'>
-            <p className="fs-6 fw-bold mt-3">{name}</p>
-            <img src={assets.rating_starts} />
+                <p className="food-item-desc">{desc}</p>
+                <p className="food-item-price mt-1 fw-bold">${price}</p>
+            </div>
         </div>
-        <p className='food-item-desc'>{desc}</p>
-        <p className='food-item-price mt-1 fw-bold'>${price}</p>
-      </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default FoodItem
+export default FoodItem;
